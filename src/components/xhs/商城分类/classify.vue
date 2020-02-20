e23c8a3134683270f786ae9658f0551ba31fb8b5
lijiajun <1305551599qq.com>
2020-02-21 00:35:24 +0800

    junjun第一次提交

---
+++ src/components/xhs/商城分类/classify.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+      <p align="center"><img align="left" src="./img/微信截图_20200220231152.png" alt="">分类</p>
+      <hr>
+      <div class="a1">
+          <div class="d1">
+            <ul>
+              <li>护肤</li>
+              <li>护肤</li>  
+              <li>护肤</li>  
+              <li>护肤</li>  
+              <li>护肤</li>  
+              <li>护肤</li>    
+            </ul>   
+          </div>
+          <div class="d2">
+
+          </div>
+      </div>
+    </div>   
+</template>
+<style>
+  p{
+      font-size: 30px;
+      text-align: center;
+  }
+  .a1{
+      width: 100%;
+      display: flex;
+      background-color: #666;
+  }
+  .d1{
+      background-color: #fff;
+      width: 20%;
+  }
+  .d2{ background-color: #fff;
+      margin-left: 5px;
+      margin-top: 5px;
+      width: 80%;
+  }
+</style>
+<script>
+
+</script>>
